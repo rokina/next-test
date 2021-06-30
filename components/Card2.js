001acf5a833233d648b1dbf589ac3bf34e3ba3ea
hk-sato <hk-sato@respect-pal.jp>
2021-06-30 12:30:16 +0900

    fix: Card2 画像がない場合の画像設定

--- components/Card2.js
+++ components/Card2.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import styles from '../styles/Card2.module.scss';
 
 const Card2 = (props) => {
+  const default_image = "https://images.microcms-assets.io/assets/49f98e2075bc4b10af6d727b1efa9c2b/cca3e5b7438c4f2bbebb56fe2ccd6444/42589447.webp"
   return (
     <div>
       {props.news.map(news => (
@@ -9,7 +10,7 @@ const Card2 = (props) => {
           <Link href={`/news/${news.id}`}>
             <a className={styles.card__item}>
               <span className={styles.card__label}>{news.category.name}</span>
-              <img src={news.mainVisual && news.mainVisual.url} alt="" className={styles.card__thumbnail} />
+              <img src={news.mainVisual && news.mainVisual.url ? news.mainVisual.url : default_image} alt="" className={styles.card__thumbnail} />
               <div className={styles.card__content}>
                 <time className={styles.card__date}>
                   {news.publishedAt}
@@ -22,50 +23,6 @@ const Card2 = (props) => {
           </Link>
         </article>
       ))}
-      {/* <article className={styles.card}>
-        <Link href={`/news/${news.id}`}>
-          <a>{news.title}</a>
-        </Link>
-      </article> */}
-      {/* <ul>
-        {props.news.map(news => (
-          <li key={news.id}>
-            <Link href={`/news/${news.id}`}>
-              <a>{news.title}</a>
-            </Link>
-            <p>{news.body}</p>
-          </li>
-        ))}
-      </ul> */}
-      {/* <article class="c-card" v-for="content in postsData" :key="content.id">
-        <nuxt-link :to="`/${content.id}`" class="c-card__item">
-          <span class="c-card__label">{{
-            content.category && content.category.name
-          }}</span>
-          <img
-            v-if="content.mainVisual"
-            :src="content.mainVisual.url"
-            alt=""
-            class="c-card__thumbnail"
-          />
-          <img
-            v-else
-            src="https://images.microcms-assets.io/assets/49f98e2075bc4b10af6d727b1efa9c2b/cca3e5b7438c4f2bbebb56fe2ccd6444/42589447.webp"
-            alt=""
-            class="c-card__thumbnail"
-          />
-          <div class="c-card__content">
-            <time
-              class="c-card__date"
-              :datetime="$moment(content.publishedAt).format('YYYY-MM-DD')"
-              >{{ $moment(content.publishedAt).format('YYYY.MM.DD') }}</time
-            >
-            <p class="c-card__title">
-              {{ content.title }}
-            </p>
-          </div>
-        </nuxt-link>
-      </article> */}
     </div>
   );
 }
