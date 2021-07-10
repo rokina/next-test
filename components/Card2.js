d6e04f4e972711b84682665f437853c064f7ccb7
hiroki <raxito2008@yahoo.co.jp>
2021-07-10 12:29:30 +0900

    fix 日付フォーマット処理完成

--- components/Card2.js
+++ components/Card2.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import styles from '../styles/Card2.module.scss';
+import { format } from 'date-fns';
 
 const Card2 = (props) => {
   const default_image = "https://images.microcms-assets.io/assets/49f98e2075bc4b10af6d727b1efa9c2b/cca3e5b7438c4f2bbebb56fe2ccd6444/42589447.webp"
 
-  const dateFormat = (date) => {
-    return new Date(date);
+  const dateFormat = (date, format_type) => {
+    return format(new Date(date), format_type);
   };
 
   return (
@@ -17,10 +18,8 @@ const Card2 = (props) => {
               <span className={styles.card__label}>{news.category.name}</span>
               <img src={news.mainVisual && news.mainVisual.url ? news.mainVisual.url : default_image} alt="" className={styles.card__thumbnail} />
               <div className={styles.card__content}>
-                <time className={styles.card__date}>
-                  {console.log(news.publishedAt)}
-                  {console.log(new Date(news.publishedAt))}
-                  {console.log(dateFormat(news.publishedAt))}
+                <time className={styles.card__date} dateTime={dateFormat(news.publishedAt, 'yyyy-MM-dd')}>
+                  {dateFormat(news.publishedAt, 'yyyy.MM.dd')}
                 </time>
                 <p className={styles.card__title}>
                   {news.title}
