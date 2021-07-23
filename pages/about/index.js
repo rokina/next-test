0a52e963a3132b64ae4c1ef9e99453db6cf355d8
hiroki <raxito2008@yahoo.co.jp>
2021-07-23 15:30:32 +0900

    add ダミーページ追加

---
+++ pages/about/index.js
@@ -0,0 +1,14 @@
+import { Layout, Footer } from '/components/Index';
+
+const Home = () => {
+  return (
+    <Layout>
+      <main>
+        <h2>会社概要</h2>
+      </main>
+      <Footer />
+    </Layout>
+  );
+}
+
+export default Home
---
+++ pages/privacy-policy/index.js
@@ -0,0 +1,14 @@
+import { Layout, Footer } from '/components/Index';
+
+const Home = () => {
+  return (
+    <Layout>
+      <main>
+        <h2>プライバシーポリシー</h2>
+      </main>
+      <Footer />
+    </Layout>
+  );
+}
+
+export default Home
